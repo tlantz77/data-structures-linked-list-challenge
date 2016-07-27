a7047fa6440448ac221e3c4f968d0a1ab9eee763
tlantz77 <tlantz@gmail.com>
2016-07-27 17:37:18 -0500

    insert and remove methods for linked list

--- linked_list.rb
+++ linked_list.rb
@@ -2,12 +2,52 @@ require_relative 'node'
 
 class LinkedList
 
-  attr_accessor :size
+  attr_reader :size, :first, :last
 
   def initialize
     @size = 0
+    @first = nil
+    @last = nil
   end
 
-  
+  def get_node(index)
+    node = @first
+    index.times do
+      node = node.next
+    end
+    node
+  end
+
+  def get(index)
+    get_node(index).element
+  end
+
+  def insert_first(element)
+    new_node = Node.new(element)
+    @first = new_node
+    @last = new_node if @last == nil
+    @size += 1
+  end
+
+  def remove_first
+    return nil if @first ==  nil
+    @first = @first.next
+    @size -= 1
+  end
+
+  def insert_last(element)
+    new_node = Node.new(element)
+    @last = new_node
+    @first = new_node if @first == nil
+    @size += 1
+    @first.next = @last if @size == 2
+  end
+
+  def remove_last
+    return nil if @last == nil
+    @size -= 1
+    @last = get_node(@size - 1)
+    @last.next = nil
+  end
 
 end
--- linked_list_spec.rb
+++ linked_list_spec.rb
@@ -2,4 +2,16 @@ require_relative 'linked_list'
 
 describe LinkedList do
 
+  let(:linked_list) { LinkedList.new }
+
+  it 'can set first and last elements' do
+    linked_list.insert_first("first")
+    linked_list.insert_last("last")
+    expect(linked_list.first.element).to eq("first")
+    expect(linked_list.last.element).to eq("last")
+    expect(linked_list.first.next.element).to eq("last")
+    expect(linked_list.last.next).to eq(nil)
+    expect(linked_list.size).to eq(2)
+  end
+
 end
