ae3a8097a4d039cb50818dff11cf71a26b780f7f
tlantz77 <tlantz@gmail.com>
2016-07-28 17:24:14 -0500

    add and test insert function

--- linked_list.rb
+++ linked_list.rb
@@ -11,6 +11,7 @@ class LinkedList
   end
 
   def get_node(index)
+    check_bounds(index)
     node = @first
     index.times do
       node = node.next
@@ -19,11 +20,13 @@ class LinkedList
   end
 
   def get(index)
+    check_bounds(index)
     get_node(index).element
   end
 
   def insert_first(element)
     new_node = Node.new(element)
+    new_node.insert_after(@first)
     @first = new_node
     @last = new_node if @last == nil
     @size += 1
@@ -37,6 +40,7 @@ class LinkedList
 
   def insert_last(element)
     new_node = Node.new(element)
+    @last.insert_after(new_node)
     @last = new_node
     @first = new_node if @first == nil
     @size += 1
@@ -48,6 +52,30 @@ class LinkedList
     @size -= 1
     @last = get_node(@size - 1)
     @last.next = nil
+  end
+
+  def insert(index, element)
+    check_bounds(index)
+
+    if index == 0
+      self.insert_first(element)
+    else
+      new_node = Node.new(element)
+      node = @first
+      prior_node = nil
+      index.times do
+        prior_node = node
+        node = node.next
+      end
+      prior_node.insert_after(new_node)
+      new_node.insert_after(node)
+      @size += 1
+    end
+
+  end
+
+  def check_bounds(index)
+    raise StandardError, 'Out of Bounds' if index > @size - 1
   end
 
 end
--- linked_list_spec.rb
+++ linked_list_spec.rb
@@ -4,14 +4,46 @@ describe LinkedList do
 
   let(:linked_list) { LinkedList.new }
 
-  it 'can set first and last elements' do
+  before(:each) do
     linked_list.insert_first("first")
     linked_list.insert_last("last")
+  end
+
+  it 'can get the size of the list' do
+    expect(linked_list.size).to eq(2)
+  end
+
+  it 'can set first and last elements' do
     expect(linked_list.first.element).to eq("first")
     expect(linked_list.last.element).to eq("last")
     expect(linked_list.first.next.element).to eq("last")
     expect(linked_list.last.next).to eq(nil)
     expect(linked_list.size).to eq(2)
+  end
+
+  it 'can remove first element' do
+    linked_list.remove_first
+    expect(linked_list.first.element).to eq("last")
+    expect(linked_list.size).to eq(1)
+  end
+
+  it 'can retrieve an element at a given index' do
+    expect(linked_list.get(0)).to eq("first")
+    expect(linked_list.get(1)).to eq("last")
+  end
+
+  it 'will throw error to trying to get element from index outside of bounds' do
+    expect { linked_list.get(2) }.to raise_error(StandardError, 'Out of Bounds')
+  end
+
+  it 'can insert element at a given index' do
+    linked_list.insert(1, "second")
+    linked_list.insert(2, "third")
+    expect(linked_list.size).to eq(4)
+    expect(linked_list.get(0)).to eq("first")
+    expect(linked_list.get(1)).to eq("second")
+    expect(linked_list.get(2)).to eq("third")
+    expect(linked_list.get(3)).to eq("last")
   end
 
 end
